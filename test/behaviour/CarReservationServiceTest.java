ab65061a124ab491848095cdacc6b498cca6c5fe
Anton Stamme <anton@stamme.de>
2021-06-01 21:35:28 +0200

    added a simple junit test

---
+++ test/behaviour/CarReservationServiceTest.java
@@ -0,0 +1,18 @@
+package behaviour;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class CarReservationServiceTest {
+
+    @BeforeAll
+    private static void setUp() {
+
+    }
+
+    @Test
+    protected void canCarBeReserved() {
+        //TODO: implement useful test
+    }
+}
+
