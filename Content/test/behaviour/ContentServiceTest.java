ede39662924b5f7fcf4dd640aeabd8876c7cbdb3
Anton Stamme <anton@stamme.de>
2021-06-18 16:46:12 +0200

    Initialized Content module and added test class

---
+++ Content/test/behaviour/ContentServiceTest.java
@@ -0,0 +1,17 @@
+package behaviour;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ContentServiceTest {
+
+    @BeforeAll
+    private static void setUp() {
+
+    }
+
+    @Test
+    protected void canContentBeAdded() {
+        //TODO: implement useful test
+    }
+}
