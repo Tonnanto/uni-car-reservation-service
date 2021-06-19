9b0a6d82895d09f648affc572fe8cf850f27ad8a
Anton Stamme <anton@stamme.de>
2021-06-19 13:53:06 +0200

    Implemented tests for ContentService

--- Content/test/behaviour/ContentServiceTest.java
+++ Content/test/behaviour/ContentServiceTest.java
@@ -1,17 +1,72 @@
 package behaviour;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import structure.BookingFile;
+import structure.Content;
+import structure.Folder;
+
+import java.time.LocalDate;
+import java.util.Map;
 
 public class ContentServiceTest {
 
-    @BeforeAll
-    private static void setUp() {
+    private static ContentService contentService;
 
+    @BeforeAll
+    protected static void setUp() {
+        contentService = new ContentService();
+    }
+
+    @AfterAll
+    protected static void tearDown() {
+        // Outputting hierarchy to console
+        System.out.println(contentService.getRoot());
     }
 
     @Test
     protected void canContentBeAdded() {
-        //TODO: implement useful test
+
+        // Adding test files to hierarchy
+        Assertions.assertTrue(contentService.addContent(new BookingFile(), LocalDate.of(2017, 1, 1)));
+        Assertions.assertTrue(contentService.addContent(new BookingFile(), LocalDate.of(2017, 5, 23)));
+        Assertions.assertTrue(contentService.addContent(new BookingFile(), LocalDate.of(2018, 12, 31)));
+        Assertions.assertTrue(contentService.addContent(new BookingFile(), LocalDate.of(2018, 12, 31)));
+
+        Assertions.assertTrue(contentService.addContent(new BookingFile()));
+        Assertions.assertTrue(contentService.addContent(new BookingFile()));
+        Assertions.assertTrue(contentService.addContent(new BookingFile()));
+        Assertions.assertTrue(contentService.addContent(new BookingFile()));
+        Assertions.assertTrue(contentService.addContent(new BookingFile()));
+        Assertions.assertTrue(contentService.addContent(new BookingFile()));
+    }
+
+    @Test
+    protected void cantSameFilesBeAdded() {
+        Assertions.assertTrue(contentService.addContent(new BookingFile("Booking_abc")));
+        // Files with same name can't be added to the same Folder
+        Assertions.assertFalse(contentService.addContent(new BookingFile("Booking_abc")));
+    }
+
+    @Test
+    protected void areSummaryFilesBeingCreated () {
+        Folder root = contentService.getRoot();
+        Assertions.assertNotNull(root.getContent(root.getName() + "_Summary"));
+
+        for (Map.Entry<String, Content> yearEntry: root.getContents().entrySet()) {
+            if (yearEntry.getValue() instanceof Folder) {
+                Folder yearFolder = (Folder) yearEntry.getValue();
+                Assertions.assertNotNull(yearFolder.getContent(yearFolder.getName() + "_Summary"));
+
+                for (Map.Entry<String, Content> monthEntry: root.getContents().entrySet()) {
+                    if (monthEntry.getValue() instanceof Folder) {
+                        Folder monthFolder = (Folder) monthEntry.getValue();
+                        Assertions.assertNotNull(monthFolder.getContent(monthFolder.getName() + "_Summary"));
+                    }
+                }
+            }
+        }
     }
 }
