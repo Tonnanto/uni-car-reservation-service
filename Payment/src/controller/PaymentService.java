6c3927633d90da1ec9c867d068375e448ac2713f
Anton Stamme <anton@stamme.de>
2021-06-22 13:30:46 +0200

    implemented "payAmount" UseCase

--- Payment/src/controller/PaymentService.java
+++ Payment/src/controller/PaymentService.java
@@ -6,7 +6,47 @@ public class PaymentService {
 
     public boolean payAmount(CurrencyAmount amount) {
 
+        PaymentType paymentType = null;
+        Payment payment = null;
+
+        // TODO: Prompt customer to choose PaymentType
+
+        if (paymentType == null)
+            return false;
+
+        // create payment
+        payment = switch (paymentType) {
+            case PAYPAL -> new PayPalPayment(amount);
+            case GOOGLE_WALLET -> new GoogleWalletPayment(amount);
+            case MONEY_WALLET -> new MoneyWalletPayment(amount);
+        };
+        payment.setReceiverAccount(getReceiverAccount());
+
+        // TODO: Prompt customer to enter credentials for PaymentType
+        String userName = "";
+        String password = "";
+
+        // 1. authorize customer
+        boolean isUserAuthorized = payment.authorizeCustomer(userName, password);
+
+        // TODO: Show user authorization status
+        // TODO: (maybe) prompt final payment confirmation from user
+
+        // 2. pay amount
+        boolean isAmountPayed = payment.payAmount();
+
+        // 3. create confirmation
+        String confirmation = payment.createConfirmation(isAmountPayed);
+
+        // TODO: Show confirmation to customer
+
         return true;
+    }
+
+    private Account getReceiverAccount() {
+        String ownerName = "Car Reservation Service GmbH";
+        String address = "carreservation@service.de";
+        return new Account(ownerName, address);
     }
 
 }
