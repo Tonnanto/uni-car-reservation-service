8578012995b28b38410242f66f944ec1eef9f572
Plploe <jonas.harms@stud.leuphana.de>
2021-06-23 14:42:31 +0200

    Added JavaDoc

--- Booking/src/behaviour/BookingDirector.java
+++ Booking/src/behaviour/BookingDirector.java
@@ -3,15 +3,19 @@ package behaviour;
 import structure.Booking;
 
 public class BookingDirector {
-    private BookingBuilder bookingBuilder;
+    private final BookingBuilder bookingBuilder;
 
     public BookingDirector(BookingBuilder bookingBuilder) {
         this.bookingBuilder = bookingBuilder;
     }
 
-    public Booking getBooking(){
+    public Booking getBooking() {
         return bookingBuilder.getBooking();
     }
+
+    /**
+     * create a new Booking with a text
+     */
 
     public void createBooking() {    //Todo Ressource & Payment einfügen und übergeben
         bookingBuilder.buildHeader();
