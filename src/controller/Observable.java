3e8b565dbb3a6d788c4ef7d7f92b5dda964cfd5e
Anton Stamme <anton@stamme.de>
2021-06-25 13:04:51 +0200

    Added Observer Classes

---
+++ src/controller/Observable.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Observable {
+    private final List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        this.observers.add(observer);
+    }
+
+    public void addObservers(List<Observer> observers) {
+        this.observers.addAll(observers);
+    }
+
+    public List<Observer> getObservers() {
+        return this.observers;
+    }
+
+    public void setChanged() {
+        this.notifyObservers();
+    }
+
+    private void notifyObservers() {
+        for(Observer observer: observers) {
+            observer.update(this);
+        }
+    }
+}
