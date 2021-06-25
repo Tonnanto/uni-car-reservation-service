a192bd17aec78e48b45eb1471a2fab19418ed011
Anton Stamme <anton@stamme.de>
2021-06-26 00:04:38 +0200

    Added methods to resource service

--- Resource/src/controller/ResourceService.java
+++ Resource/src/controller/ResourceService.java
@@ -2,28 +2,82 @@ package controller;
 
 import model.Car;
 import model.Resource;
-import model.decorator.ChildSeat;
-import model.decorator.SetTopBox;
+import view.SelectActionView;
+import view.SelectCarView;
+import view.ShowSelectedResourceView;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Manages the selection process and the creation of a resource (car).
  */
-public class ResourceService {
+public class ResourceService implements Observer {
+
+    private final List<Car> availableCars;
+    private boolean isResourceSelected;
+    private Resource resource;
+
+    public ResourceService() {
+        this.availableCars = Arrays.asList(
+                new Car("BMW 530d Touring", 49900),
+                new Car("Volkswagen Golf", 24000)
+        );
+    }
+
+    // TODO: temporary method
+    public static void main(String[] args) {
+
+        ResourceService resourceService = new ResourceService();
+
+        resourceService.getSelectedResource();
+    }
 
     /**
      * Creates and returns a resource according to the selections of a user.
-     * // TODO: The selection procedure is yet to be implemented in a view.
      * @return the selected resource
      */
     public Resource getSelectedResource() {
 
-        Car car;
-        // TODO: Let User Select Car and Decorators
+        while (!isResourceSelected) {
+            if (resource == null) {
+                // select a car
+                new SelectCarView(this).display();
+            }
 
-        // temporary
-        car = new Car("BMW 530d Touring", 49900);
-        Resource selectedResource = new SetTopBox( new ChildSeat(car));
+        }
 
-        return selectedResource;
+        return resource;
+    }
+
+    @Override
+    public void update(Object object) {
+        new ShowSelectedResourceView(this).display();
+        new SelectActionView(this).display();
+    }
+
+    public List<Car> getAvailableCars() {
+        return availableCars;
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public void selectCar(Car car) {
+        car.addObserver(this);
+        this.resource = car;
+        car.setChanged();
+    }
+
+    public void addDecorator() {
+
+    }
+
+    /**
+     * This method is being called, when the user decides to remove the selection.
+     */
+    public void removeSelection() {
+        resource = null;
     }
 }
