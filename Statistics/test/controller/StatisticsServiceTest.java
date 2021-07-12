6e3bab0429b9a04ae3e048cf8f42f97a47cbd151
Anton Stamme <anton@stamme.de>
2021-07-12 22:12:53 +0200

    Created statistics service and test classes

---
+++ Statistics/test/controller/StatisticsServiceTest.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class StatisticsServiceTest {
+
+    private static StatisticsService statisticsService;
+
+    @BeforeAll
+    protected static void setUp() {
+        statisticsService = new StatisticsService();
+    }
+
+    @Test
+    protected void canSummaryBeCreated() {
+
+    }
+
+    @Test
+    protected void canBookingsBeGotten() {
+
+    }
+}
