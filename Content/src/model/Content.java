65f7f8232439bc7f15d645a9bf12b08dd40c53fe
Anton Stamme <anton@stamme.de>
2021-07-15 15:38:39 +0200

    Made Folder visitable and observable

--- Content/src/model/Content.java
+++ Content/src/model/Content.java
@@ -1,6 +1,8 @@
 package model;
 
-public abstract class Content {
+import controller.Observable;
+
+public abstract class Content extends Observable {
     private final String name;
 
     public Content(String name) {
