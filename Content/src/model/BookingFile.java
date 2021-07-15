e73a153affd4af003f1f703a4bd1b1211673280b
Anton Stamme <anton@stamme.de>
2021-07-15 15:37:04 +0200

    Added booking attribute to BookingFile

--- Content/src/model/BookingFile.java
+++ Content/src/model/BookingFile.java
@@ -1,14 +1,28 @@
 package model;
 
+import behaviour.BookingDirector;
+import behaviour.GermanBookingBuilder;
+import structure.Booking;
+
 public class BookingFile extends File {
 
     private static int bookingCount;
+
+    private Booking booking;
 
     public BookingFile (String name) {
         super(name);
     }
     public BookingFile() {
-        // TODO: Store Booking object in this file
         super("Booking_" + ++bookingCount);
+
+        // TODO: temporary example Booking
+        BookingDirector director = new BookingDirector(new GermanBookingBuilder());
+        director.createBooking(new Car("sdf", 300.0), new PayPalPayment(new CurrencyAmount(300.0, Currency.EURO)));
+        this.booking = director.getBooking();
+    }
+
+    public Booking getBooking() {
+        return booking;
     }
 }
