e4fae2eec652afa90608e8e9f26b3625fdfc2520
Anton Stamme <anton@stamme.de>
2021-08-11 11:29:48 +0200

    added and adjusted views

--- Content/src/view/ShowContentHierarchyView.java
+++ Content/src/view/ShowContentHierarchyView.java
@@ -1,8 +1,15 @@
 package view;
 
-public class ShowContentHierarchyView extends View {
+import controller.Command;
+
+public class ShowContentHierarchyView extends StringInputView {
     @Override
     protected String getMessage() {
+        return null;
+    }
+
+    @Override
+    protected Command getCommand(String inputString) {
         return null;
     }
 }
